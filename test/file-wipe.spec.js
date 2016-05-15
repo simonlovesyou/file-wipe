251a88ed83a848e075f9e32f8efa17ffba8df5b8
simonlovesyou <simonljohansson92@gmail.com>
2016-05-15 17:51:38 +0200

    Rewrote tests, add test of callback

--- test/file-wipe.spec.js
+++ test/file-wipe.spec.js
@@ -4,22 +4,55 @@ import secureRandom from 'secure-random';
 import path from 'path';
 import bluebird from 'bluebird';
 import wipe from '../dist/file-wipe';
+import pathExists from 'path-exists';
+import glob from 'glob';
 bluebird.promisifyAll(fs);
 
-const exampleFile = path.join(process.cwd(), 'example.txt');
+//Remove all example files. 
+const _rmExampleFiles = () => glob('example\d*.txt', (err, files) => {
+  if(err) {
+    throw err;
+  }
+  files.forEach(file => fs.unlink(file));
+})
 
-test.before(async t => {
-  //Create some random data
-  let data = secureRandom(1337, {type: 'Buffer'});
-  //Create a temporary test file from the data.
-  await fs.writeFileAsync(exampleFile, data);
+//Write some data to an example file.  
+const _writeExampleFile = (file) => fs.writeFileAsync(file, secureRandom(1337, {type: 'Buffer'}))
+
+//Remove the example files if the lib fails to wipe it. 
+test.after(_rmExampleFiles);
+
+test('wiping file with promise', async t => {
+
+  let fileName = 'example1.txt';
+
+  await _writeExampleFile(fileName)
+
+  //Make sure the example file exists.
+  t.true(pathExists.sync(fileName));
+
+  await wipe(fileName)
+        .then(() => t.false(pathExists.sync(fileName)))
+        //Fail the test if file-wipe returns an error.
+        .catch(err => t.fail());
+
 });
 
-test('wiping', async t => {
+test('wiping with callback', async t => {
 
-  t.throws(
-    wipe(exampleFile)
-    .then(() => fs.statAsync(exampleFile))
-    //File should be wiped.
-  , /(ENOENT)/);
+  let fileName = 'example2.txt';
+
+  await _writeExampleFile(fileName)
+
+  //Make sure the example file exists.
+  t.true(pathExists.sync(fileName));
+
+  await wipe(fileName, function(err) {
+    if(err) {
+      //Fail the test if file-wipe returns an error.
+      return t.fail();
+    }
+    return t.false(pathExists.sync(fileName));
+  })
+
 });
